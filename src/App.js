d3fbc54c3324c49bf4c737c06e55e904063f2448
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-17 20:37:19 -0300

    Organização dos códigos dos 3 integrantes do grupo.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
 import React from 'react';
+import { products } from './products';
+import styled from 'styled-components';
+import cart from './img/cart.png';
 import {Filtro} from './components/Filtro';
+
+const ContainerPrincipal = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  min-height: 100vh;
+  row-gap: 20px;
+  column-gap: 20px;
+  text-align: center;
+  margin:10px;
+`
+const ImgSatelites = styled.img`
+  width: 100%;
+  height: 50%;
+  border-radius: 10px 10px 0 0;
+`
+
+const Cart = styled.img`
+  height: 50px;
+`
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 100px;
+  background-color: rgb(143, 183, 206);
+  border-radius: 0 0 0 30px;
+  color: black;
+`
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  grid-column: 1 /-1;
+  background-color: rgb(143, 183, 206);
+  color: rgb(14, 39, 68);
+  font-size: 20px;
+`;
+
+const CardsProdutos = styled.div`
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  box-shadow: 2px 2px 5px darkgrey;
+  border-radius: 10px;
+  background-color: lightgray;
+  margin: 10px 20px;
+  color: rgb(95, 19, 39);
+`
+const CardsTitulos = styled.h2`
+  font-size: medium;
+`
+const Button = styled.button`
+  border-radius: 15px;
+  width: 60%;
+`
 
 export default class App extends React.Component {
   state = {
@@ -38,14 +97,34 @@ buscarProdutos = () => {
 
   render() {
     return(
-      <Filtro
-        mininoValue={this.state.mininoValue}
-        maximoValue={this.state.maximoValue}
-        produtoValue={this.state.produtoValue}
-        onChangeMinimoValue={this.onChangeMinimoValue}            
-        onChangeMaximoValue={this.onChangeMaximoValue}            
-        onChangeProdutoValue={this.onChangeProdutoValue} 
-      />
-    )
-  }
-}
+      <div>
+        <Header>
+          <h1>Labe-Commerce</h1>
+          <Cart src={cart}></Cart>
+        </Header>
+        <Filtro
+          mininoValue={this.state.mininoValue}
+          maximoValue={this.state.maximoValue}
+          produtoValue={this.state.produtoValue}
+          onChangeMinimoValue={this.onChangeMinimoValue}            
+          onChangeMaximoValue={this.onChangeMaximoValue}            
+          onChangeProdutoValue={this.onChangeProdutoValue} 
+        />
+        <ContainerPrincipal>
+          {products.map((product) => {
+            return (
+              <CardsProdutos key={product.id}>
+                <ImgSatelites src={product.icone} />
+                <CardsTitulos>{product.nome}</CardsTitulos>
+                <p>R${product.preco}</p>
+                <Button>Adicionar ao carrinho</Button>
+              </CardsProdutos>
+            );
+          })}
+        </ContainerPrincipal>
+        <Footer>
+          <p> &#10049; Labe-Commerce feito com &#10084; para você</p>
+        </Footer>
+      </div>
+    );
+}};
